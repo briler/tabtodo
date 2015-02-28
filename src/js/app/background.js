6cfc273abf9ce04bf4b8b4962c016f4678dd6925
briler <briler1@gmail.com>
2015-02-28 10:13:27 +0200

    rename tab 2

---
+++ src/js/app/background.js
@@ -0,0 +1,22 @@
+function activateLock(setTitle, tabID) {
+	chrome.tabs.executeScript(tabID,
+      {code:"document.title='" + setTitle + "'"});
+	
+  var relTab = tabID;
+	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+   		if (tabId != relTab) {
+          return false;
+    	}
+   		  chrome.tabs.executeScript(tabID,
+          {code:"document.title='" + setTitle + "'"});
+  });
+}
+
+chrome.extension.onMessage.addListener(
+    alert('changed event');
+    function(request,sender,sendResponse) {
+        if (request.to == "background") {
+            activateLock(request.title, request.relTabID);
+        }
+    }
+) ;
