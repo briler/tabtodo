129fc6f3185e85b0a20e52b577f16117b71edb43
briler <briler1@gmail.com>
2015-05-15 00:36:23 +0300

    gacode

--- src/js/app/popup.js
+++ src/js/app/popup.js
@@ -1,4 +1,16 @@
-﻿
+﻿// Standard Google Universal Analytics code
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-61679448-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
+
 myApp.service('pageInfoService', function() {
     this.getInfo = function(callback) {
         var model = {};
