feb44a21d866551db0d2dfffda669daa4263e810
briler <briler1@gmail.com>
2015-07-22 23:11:23 +0300

    cont firefox work

--- ff/index.js
+++ ff/index.js
@@ -1,6 +1,8 @@
 var { ToggleButton } = require('sdk/ui/button/toggle');
 var panels = require("sdk/panel");
 var self = require("sdk/self");
+var tabs = require("sdk/tabs");
+
 
 var button = ToggleButton({
   id: "my-button",
@@ -15,9 +17,32 @@ var button = ToggleButton({
 
 var panel = panels.Panel({
   contentURL: self.data.url("popup.html"),
+  contentScriptFile: [
+  	self.data.url("js/lib/jqueryandangular.min.js"),
+  	self.data.url("js/lib/ngDialog.min.js"),
+  	self.data.url("js/lib/angular-strap.min.js"),
+  	self.data.url("js/lib/angular-strap.tpl.min.js"),
+  	self.data.url("js/app/app.js"),
+  	self.data.url("js/app/popup.js"),
+   ],
+  /*
+
+        <script src="js/lib/jqueryandangular.min.js"></script>
+        <script src="js/lib/ngDialog.min.js"></script>
+        <script src="js/lib/angular-strap.min.js"></script>
+        <script src="js/lib/angular-strap.tpl.min.js"></script>
+        
+        <script src="js/app/app.js"></script>
+        <script src="js/app/popup.js"></script>
+  */
   onHide: handleHide
 });
 
+var tabs = require('sdk/tabs');
+for (let tab of tabs)
+  console.log(tab);
+
+/**/
 function handleChange(state) {
   if (state.checked) {
     panel.show({
